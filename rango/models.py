3dc2277a4e3d69fed5cffe9a85418d253d94f701
Luca Bisin <2289370b@student.gla.ac.uk>
2017-01-26 10:03:47 +0000

    Ch. 4 serving media finished

--- rango/models.py
+++ rango/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	
+	def __str__(self):
+		return self.name
+		
+	
