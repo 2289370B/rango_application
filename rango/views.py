6d2ded30a901d700a72d4774c224d8220f1ed72f
Luca Bisin <2289370b@student.gla.ac.uk>
2017-01-26 10:17:41 +0000

    Ch. 4 - cat image

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,4 @@ def index(request):
 
 # about view
 def about(request):
-	return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index</a>")
+	return render(request,'rango/about.html')
