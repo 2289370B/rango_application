5d73f25411c2ab46332b51fed68f9f5d0da6b815
Luca Bisin <2289370b@student.gla.ac.uk>
2017-01-30 13:01:08 +0000

    Ch. 6 - exercises

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,10 @@ from rango.models import Page
 
 # index view
 def index(request):
-   category_list = Category.objects.order_by('-likes')[:5]
-   context_dict = {'categories': category_list}
-   return render(request, 'rango/index.html', context_dict)
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}
+	return render(request, 'rango/index.html', context_dict)
 
 # about view
 def about(request):
