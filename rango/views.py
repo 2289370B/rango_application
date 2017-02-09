f5ed2a437295f635574b00b9d35dab225d86b26b
Luca Bisin <2289370b@student.gla.ac.uk>
2017-02-09 10:35:02 +0000

    Ch. 10 - cookies

--- rango/views.py
+++ rango/views.py
@@ -6,17 +6,40 @@ from django.urls import reverse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
+def visitor_cookie_handler(request, response):
+	visits = int(request.COOKIES.get('visits', '1'))
+	
+	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	
+	if(datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		response.set_cookie('last_visit', str(datetime.now()))
+	else:
+		visits = 1
+		response.set_cookie('last_visit', last_visit_cookie)
+		
+	response.set_cookie('visits', visits)
 
 # index view
 def index(request):
+	request.session.set_test_cookie()
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
-	return render(request, 'rango/index.html', context_dict)
+	
+	response = render(request, 'rango/index.html', context_dict)
+	visitor_cookie_handler(request, response)
+	
+	return response
 
 # about view
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED")
+		request.session.delete_test_cookie()
 	return render(request,'rango/about.html')
 	
 # show category
