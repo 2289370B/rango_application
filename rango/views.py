50b6433c23b87724f397b053470711366af6b57f
Luca Bisin <2289370b@student.gla.ac.uk>
2017-02-09 11:08:16 +0000

    Ch. 10 - sessions

--- rango/views.py
+++ rango/views.py
@@ -8,20 +8,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
-def visitor_cookie_handler(request, response):
-	visits = int(request.COOKIES.get('visits', '1'))
-	
-	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
 	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 	
 	if(datetime.now() - last_visit_time).days > 0:
 		visits = visits + 1
-		response.set_cookie('last_visit', str(datetime.now()))
+		request.session['last_visit'] = str(datetime.now())
 	else:
 		visits = 1
-		response.set_cookie('last_visit', last_visit_cookie)
+		request.session['last_visit'] = last_visit_cookie
 		
-	response.set_cookie('visits', visits)
+	request.session['visits'] = visits
 
 # index view
 def index(request):
@@ -30,17 +35,17 @@ def index(request):
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
 	
-	response = render(request, 'rango/index.html', context_dict)
-	visitor_cookie_handler(request, response)
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	
+	response = render(request, 'rango/index.html', context_dict)
 	return response
 
 # about view
 def about(request):
-	if request.session.test_cookie_worked():
-		print("TEST COOKIE WORKED")
-		request.session.delete_test_cookie()
-	return render(request,'rango/about.html')
+	visitor_cookie_handler(request)
+	context_dict = {'visits': request.session['visits']}
+	return render(request,'rango/about.html', context_dict)
 	
 # show category
 def show_category(request, category_name_slug):
